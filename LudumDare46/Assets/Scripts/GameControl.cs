7b5446013fa5749eb01f68b57e880542c728ae9c
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2020-04-19 21:19:26 +0100

    Day 1 3/4 progress and sprites push

--- LudumDare46/Assets/Scripts/GameControl.cs
+++ LudumDare46/Assets/Scripts/GameControl.cs
@@ -10,23 +10,80 @@ public class GameControl : MonoBehaviour
     private GlobalBlackBoard gbb;
 
     public CanvasGroup HUD;
-    public CanvasGroup EndScreen;
+    public TextMeshProUGUI HUDScore;
+    public TextMeshProUGUI timerText;
 
-    public TextMeshProUGUI scoreText;
+    public CanvasGroup EndScreen;
+    public TextMeshProUGUI EndScore;
+
+    private float gameTimer;
+    public float gameLength;
+
+    private bool gameRunning = true;
     // Start is called before the first frame update
     void Start()
     {
         gbb = GameObject.FindGameObjectWithTag("GBB").GetComponent<GlobalBlackBoard>();
+
+        HideCanvasGroup(EndScreen);
+        ShowCanvasGroup(HUD);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        score += (int)gbb.scoreDelta;
+        if(gameRunning)
+        {
+            score += (int)gbb.scoreDelta;
+
+            HUDScore.text = score.ToString();
+
+            gameTimer += Time.deltaTime;
+
+
+            if (gameTimer >= gameLength)
+            {
+                EndGame();
+            }
+        }
+        else
+        {
+
+        }
+        string timeText;
+        int minutes = (int)(gameLength - gameTimer) / 60;
+        int seconds = (int)(gameLength - gameTimer) % 60;
+
+        timeText = minutes.ToString() + ": " + seconds.ToString();
+        timerText.text = timeText;
     }
 
     public void EndGame()
     {
+        gameRunning = false;
 
+        GameObject[] FFs = GameObject.FindGameObjectsWithTag("FireFighter");
+        foreach(GameObject FF in FFs)
+        {
+            FF.SetActive(false);
+        }
+
+        HideCanvasGroup(HUD);
+        ShowCanvasGroup(EndScreen);
+        EndScore.text = score.ToString();
+    }
+
+    private void HideCanvasGroup(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 0f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    private void ShowCanvasGroup(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
     }
 }
